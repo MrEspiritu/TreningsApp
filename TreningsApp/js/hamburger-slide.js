f9484311b7ab06755e18fbc87c0af7533499affd
BennyHoang <hoaben13@nith.no>
2015-05-26 23:10:02 +0200

    slide animation faster

--- TreningsApp/js/hamburger-slide.js
+++ TreningsApp/js/hamburger-slide.js
@@ -2,7 +2,7 @@ $(function() {
 
     function settEvent() {
         $(".cmn-toggle-switch").click(function() {
-            $("#ham-menu-open").animate({width: 'toggle'});
+            $("#ham-menu-open").animate({width: 'toggle'}, 200);
             $(this).toggleClass('active');
         });
     }
